23f53b99d1cb9b33efed1bb8751d1391ab2fa043
Anuya Karnik <aakarnik8@gmail.com>
2021-04-04 14:37:20 -0400

    working on search

--- novelish/src/comp/Search.js
+++ novelish/src/comp/Search.js
@@ -2,10 +2,22 @@ import React, {Component}  from 'react';
 import '../App.css';
 
 function Search() {
+
+
   return (
     <div className="search">
         <h3>Space for the Search Bar.</h3>
         <p>Possibly can be used to render the search results</p>
+        <div className="search-bar">
+          <form action="">
+            <input 
+            type="text"
+            placeholder="Search..."/>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+
+
     </div>
   );
 }
