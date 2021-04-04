34e302560b594352a91f5eb6c740f307bc784bd3
Nikita Badhwar <nikitabadhwar@Nikitas-MacBook-Pro.local>
2021-04-04 16:38:35 -0400

    multi pagesgit status

--- novelish/src/App.js
+++ novelish/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import Home from './comp/Home';
 import Nav from './comp/Nav';
 import Search from './comp/Search'
 import './App.css';
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import  Home  from "./components/home";
+import  About  from "./components/about";
+import Navigation from "./components/navigation";
 /*
 My only question is how to change the 'home' components to 'search' when the user
 uses the search tab
@@ -11,11 +13,17 @@ uses the search tab
 function App() {
   return (
     <div className="App">
-      <Nav/>
-      <Search/>
-      <Home/>
+      <Router>
+        <Navigation />
+        <Switch>
+          <Route path="/" exact component={() => <Home />} />
+          <Route path="/about" exact component={() => <About />} />
+        </Switch>
+      </Router>
+
     </div>
   );
 }
 
 export default App;
+
---
+++ novelish/src/components/about.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="about">
+        <div class="jumbotron text-center">
+            <h1>About page</h1>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 text-center">
+                <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy text
+                ever since the 1500s, when an unknown printer took a galley of
+                type and scrambled it to make a type specimen book.
+                </p>
+
+                </div>
+            </div>
+        </div>
+
+            
+
+    </div>
+  );
+}
+
+export default About;
