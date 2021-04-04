0eeeaff2fb5d8bf5b96067f84a5802bbbb51a8b8
Anuya Karnik <aakarnik8@gmail.com>
2021-04-04 03:28:24 -0400

    comment

--- novelish/src/App.js
+++ novelish/src/App.js
@@ -4,6 +4,10 @@ import Nav from './comp/Nav';
 import Search from './comp/Search'
 import './App.css';
 
+/*
+My only question is how to change the 'home' components to 'search' when the user
+uses the search tab
+*/
 function App() {
   return (
     <div className="App">
