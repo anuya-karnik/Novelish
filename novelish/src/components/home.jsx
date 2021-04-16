6ba17f7002e4f30f11ee517f3740605aef66e067
Anuya Karnik <aakarnik8@gmail.com>
2021-04-16 19:19:13 -0400

    adding to-read list

--- novelish/src/components/home.jsx
+++ novelish/src/components/home.jsx
@@ -80,7 +80,48 @@ function Home() {
         </div>
 
       </div>
+
+      <div className="home container">
+        <div className="row">
+        <div className="currentread col-4">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Currently Reading</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Space for displaying Info about Currently read book</h6>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+          <div className="toread col-8">
+            <div className="list-controls container">
+              <div className="col"><h3>My To Read List</h3></div>
+              <div className="col"> <button type="button" class="btn btn-primary">Add New Book</button>
+              <button type="button" class="btn btn-primary">See Books to Read</button>
+              <button type="button" class="btn btn-primary">See Books Completed</button>
+            </div> 
+            </div>
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Book title</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <button type="button" class="btn btn-primary">Currently Reading</button>
+                <button type="button" class="btn btn-danger">Remove from List </button>
+                <button type="button" class="btn btn-success">Done Reading!</button>
+              </div>
+            </div>
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Book title</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+
   );
 }
 
